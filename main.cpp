bda188486d326de41c49f7f1723559b1a51433d0
Антон Шмидт <schmidt.ajax@gmail.com>
2021-12-24 23:06:49 +0700

    Optimization: Proxy access instead of adding leading zeroes

--- main.cpp
+++ main.cpp
@@ -1,28 +1,45 @@
 #include <cassert>
 #include <iostream>
 #include <string>
+#include <string_view>
 
 using namespace std;
+
+struct Proxy {
+  string_view s;
+  size_t offset;
+
+  char operator[](size_t pos) {
+    int i = pos - offset;
+    return (0 <= i) ? s[i] : '0';
+  }
+};
 
 class Solution {
  public:
   string addBinary(string a, string b) {
-    int l = std::max(a.size(), b.size());
-    a.insert(0, l - a.size(), '0');
-    b.insert(0, l - b.size(), '0');
-    for (int i = l - 1; 0 < i; --i) {
-      if (_addWithCarry(a[i], b[i])) {
-        _incrementBit(a[i-1]);
-      }
+    if (a.size() <= b.size()) {
+      return _add(b, a);
+    } else {
+      return _add(a, b);
     }
-    if (_addWithCarry(a.front(), b.front())) {
-      a.insert(0, 1, '1');
-    }
-
-    return a;
   }
 
  private:
+  string &_add(string &result, const string &arg) {
+    Proxy parg{arg, result.size() - arg.size()};
+    for (int i = result.size() - 1; 0 < i; --i) {
+      if (_addWithCarry(result[i], parg[i])) {
+        _incrementBit(result[i - 1]);
+      }
+    }
+    if (_addWithCarry(result[0], parg[0])) {
+      result.insert(0, 1, '1');
+    }
+
+    return result;
+  }
+
   int _toInt(char c) {
     return c - 0x30;
   }
@@ -31,11 +48,11 @@ class Solution {
     return val + 0x30;
   }
 
-  void _incrementBit(char& c) {
+  void _incrementBit(char &c) {
     c = _fromInt(_toInt(c) + 1);
   }
 
-  bool _addWithCarry(char& a, const char& b) {
+  bool _addWithCarry(char &a, const char &b) {
     int val = _toInt(a) + _toInt(b);
     bool carry = false;
     if (1 < val) {
