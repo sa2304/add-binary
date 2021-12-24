0315fc5f18d22f4f15d82201c937892e05ec9572
Антон Шмидт <schmidt.ajax@gmail.com>
2021-12-24 21:52:24 +0700

    Solution: insert leading zeroes

--- main.cpp
+++ main.cpp
@@ -7,8 +7,44 @@ using namespace std;
 class Solution {
  public:
   string addBinary(string a, string b) {
-    // FIXME
-    return {};
+    int l = std::max(a.size(), b.size());
+    a.insert(0, l - a.size(), '0');
+    b.insert(0, l - b.size(), '0');
+    for (int i = l - 1; 0 < i; --i) {
+      if (_addWithCarry(a[i], b[i])) {
+        _incrementBit(a[i-1]);
+      }
+    }
+    if (_addWithCarry(a.front(), b.front())) {
+      a.insert(0, 1, '1');
+    }
+
+    return a;
+  }
+
+ private:
+  int _toInt(char c) {
+    return c - 0x30;
+  }
+
+  char _fromInt(int val) {
+    return val + 0x30;
+  }
+
+  void _incrementBit(char& c) {
+    c = _fromInt(_toInt(c) + 1);
+  }
+
+  bool _addWithCarry(char& a, const char& b) {
+    int val = _toInt(a) + _toInt(b);
+    bool carry = false;
+    if (1 < val) {
+      val %= 2;
+      carry = true;
+    }
+    a = _fromInt(val);
+
+    return carry;
   }
 };
 
